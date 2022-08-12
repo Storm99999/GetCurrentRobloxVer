2f28339bed833cf4dbf9a0c5723a0e2ec9595ce7
Storm <87811650+Storm99999@users.noreply.github.com>
2022-08-12 18:47:33 +0200

    Add files via upload

---
+++ GetCurrentRobloxVersion/GetCurrentRobloxVersion/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetCurrentRobloxVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Keep in mind this is NOT api-based. This app will not grab the latest version UNLESS the local-user has it installed. It's just a thing to add to a executor or something.
+            Console.WriteLine("[FINDER] Roblox Version Grabber Through LocalAppData");
+            Console.WriteLine("Hello! Welcome! This is a simplistic Roblox Version grabber, if you need it for some reason. Anyways, you can take the source of this program, and implement it into yours.");
+            var lad = $"C:\\Users\\{Environment.UserName}\\AppData\\Local\\Roblox\\Versions";
+            Console.WriteLine("[FOUND] LocalAppData was found!");
+
+            foreach (var flds in Directory.GetDirectories(lad))
+            {
+                // most likely wont happen, but just to make sure. 
+                var i = new DirectoryInfo(flds);
+                if (i.GetFiles().Length == 0 || i.GetDirectories().Length == 0)
+                {
+                    Console.WriteLine("[FINDER] Found corrupted version: " + i.FullName);
+                }
+
+                if (i.GetFiles().Length != 0)
+                {
+                    // Checks for builtinplugins, it's a folder, when that roblox ver is outdated
+                    foreach (var fe in Directory.GetFiles(i.FullName))
+                    {
+                        var fileInfo1 = new FileInfo(fe);
+                        if (Directory.Exists(i.FullName + "BuiltInPlugins"))
+                        {
+                            Console.WriteLine("[FINDER] Found old version: " + i.FullName);
+                        }
+                    }
+                }
+
+                if (File.Exists(i.FullName + "\\RobloxPlayerBeta.exe"))
+                {
+                    Console.WriteLine("[RESULTS] Found Latest Version! - " + i.FullName);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
